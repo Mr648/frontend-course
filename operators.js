f513ca064a90a81929188e028d3c45e6a078a921
mr648 <rahmatwaisi@gmail.com>
2022-02-12 20:08:13 +0330

    added javascript to html

---
+++ operators.js
@@ -0,0 +1,58 @@
+
+
+let number_1 = 100;
+let number_2 = 200;
+let number_3 = 3;
+
+// Arithmetic Operator
+
+let sum = number_1 + number_2; // 300
+
+let sub = number_1 - number_2; // -100
+
+let multiply = number_1 * number_2; // 20000
+
+let power = number_1 ** 2; // 10000
+let exponentiation  = number_1 ^ 2; // 10000 ~~~
+
+let division = number_1 / number_2; // 0.5
+
+let reminder = number_1 % number_3; // 1
+// 5 ÷ 2 = 2, reminder = 1
+
+number_3++; // added 1 to 3 = 4
+number_3--; // 3
+
+
+
+// Assignment Operators
+
+let num4 = 101;
+num4 += 10; //  num4 = num4 + 10;
+num4 *= 10; //  num4 = num4 * 10;
+num4 /= 10; //  num4 = num4 / 10;
+num4 -= 10; //  num4 = num4 - 10;
+
+num4 **= 10; // num4 = num4 ** 10;
+num4 ^= 10; //  num4 = num4 ^ 10;
+
+num4 %= 10; //  num4 = num4 % 10;
+
+
+
+// Comparison Operators
+
+let number_grater = 1000;
+let number_lower = 500;
+let result;
+
+result = number_grater > number_lower; // true 1
+result = number_grater >= number_lower; // true 1
+result = number_grater < number_lower; // false 0
+result = number_grater <= number_lower; // false 0
+
+result = number_grater == number_lower; // false 0
+result = number_grater === number_lower; // false 0
+
+result = number_grater != number_lower; // true
+result = number_grater !== number_lower; // true
