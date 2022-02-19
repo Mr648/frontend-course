14c8eb42ecba001d81482726d8936c72e2a46f73
mr648 <rahmatwaisi@gmail.com>
2022-02-19 20:41:59 +0330

    added some styles

--- resource/js/cart-list.js
+++ resource/js/cart-list.js
@@ -7,22 +7,25 @@ let carts = [
     /* Index 0 */{
         name: "Rahmat",
         phone: "08733451234",
-        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fpreviews.123rf.com%2Fimages%2Faquir%2Faquir1311%2Faquir131100316%2F23569861-%25EC%2583%2598%25ED%2594%258C-%25EC%25A7%2580-%25EB%25B9%25A8%25EA%25B0%2584%25EC%2583%2589-%25EB%259D%25BC%25EC%259A%25B4%25EB%2593%259C-%25EC%258A%25A4%25ED%2583%25AC%25ED%2594%2584.jpg&imgrefurl=https%3A%2F%2Fwww.123rf.com%2Fphoto_23569861_sample-grunge-red-round-stamp.html&tbnid=SygWekdULxI0FM&vet=12ahUKEwjzn5TGmoz2AhUGvBoKHbgQAHoQMygCegUIARDbAQ..i&docid=mFAc4MhHhMHJNM&w=1300&h=1300&q=sample%20image&ved=2ahUKEwjzn5TGmoz2AhUGvBoKHbgQAHoQMygCegUIARDbAQ",
+        image: "/resource/images/03.jpg",
         description: "This is sample"
     },
     /* Index 1 */{
         name: "Karo Ahmadi",
         phone: "08733451234",
+        image: "/resource/images/04.jpg",
         description: "This is sample"
     },
     /* Index 2 */{
         name: "Soshians Vaziri",
         phone: "08733451234",
+        image: "/resource/images/07.jpg",
         description: "This is sample"
     },
     /* Index 3 */{
         name: "Arshia Ahmadpanah",
         phone: "08733451234",
+        image: "/resource/images/09.jpg",
         description: "This is sample"
     }
 ];
@@ -33,15 +36,16 @@ for (let index = 0; index < carts.length; index++) {
     let cart = carts[index];
 
 
-
     let start = '<div class="cart">';
     let image = "";
 
-    if (index == 0){
-         image = '<img src="' + cart.image+ '">'
-    }
+    /*if (index % 2 === 0){
+         image = '<img class="img" src="' + cart.image+ '">'
+    }*/
+
 
     let name = '<h2 class="title">' + cart.name + '</h2>';
+    image = '<img class="img" src="' + cart.image + '">'
     let phone = '<a class="phone" href="tel:'
         + cart.phone
         + '">'
@@ -53,7 +57,7 @@ for (let index = 0; index < carts.length; index++) {
 
     let html = start
         + name
-    +image
+        + image
         + description
         + phone
         + end;
